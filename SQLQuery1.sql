673ef1dbb56e44d5621b49f24bb2468800bae92f
JuanDavidDava2 <juandm417@gmail.com>
2024-04-10 11:59:19 -0500

    Initial Proyect

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿use Supermarker
+
+create table PayMode
+
+(
+Pay_Mode_id int identity (100000, 1) primary key,
+Pay_Mode_Name nvarchar (50) not null,
+Pay_mode_Observation nvarchar (50) not null,
+)
+
+go
+insert into PayMode values ('Cash', 'Cash mode')
+insert into PayMode values ('Credit card','Credit card mode')
