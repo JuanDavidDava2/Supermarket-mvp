cbe6490882dd4a0c5e51d78c222804cc669bb17b
JuanDavidDava2 <juandm417@gmail.com>
2024-04-15 15:23:51 -0500

    Pay Mode Presenter

--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -1,11 +1,60 @@
 ﻿namespace Supermarket_mvp.Views
 {
-	public partial class PayModeView : Form
+	public partial class PayModeView : Form, IPayModeView
 	{
+		private string message;
+		private bool isSuccessful;
+		private bool isEdit;
+
 		public PayModeView()
 		{
 			InitializeComponent();
 		}
+
+		public string PayModeId
+		{
+			get { return TxtPayModel.Text; }
+			set { TxtPayModel.Text = value; }
+
+		}
+		public string PayModeName
+		{
+
+			get { return TxtPayModeName.Text; }
+			set { TxtPayModeName.text = value; }
+		}
+		public string PayModeObservation 
+		{
+			get { return TxtPayModeObservation.Text; }
+			set { TxtPayModeObservation.Text = value; }
+		}
+		public string SearchValue 
+		{
+			get { return TxtSearch.Text;  }
+			set { TxtSearch = value; } 
+		}
+		public bool IsEdit 
+		{
+			get { return isEdit; }
+
+			set { isEdit = value; } 
+		}
+		public bool IsSuccessful 
+		{
+			get { return isSuccessful; }
+			set { isSuccessful = value; }
+		} 
+		public string Message {
+			get { return message; }
+			set { message = value; } 
+		}
+
+		public event EventHandler SearchEvent;
+		public event EventHandler AddNewEvent;
+		public event EventHandler EditEvent;
+		public event EventHandler DeleteEvent;
+		public event EventHandler SaveEvent;
+		public event EventHandler CancelEvent;
 
 		private void label1_Click(object sender, EventArgs e)
 		{
@@ -20,6 +69,11 @@
 		private void BtnCancel_Click(object sender, EventArgs e)
 		{
 
+		}
+
+		public void SetPayModeListBildingSource(BindingSource payModeList)
+		{
+			DgPayMode.DataSource = payModeList;
 		}
 	}
 }
--- obj/Debug/net6.0-windows7.0/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows7.0/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673ef1dbb56e44d5621b49f24bb2468800bae92f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc385dd8edfd41431a4a306d5dcebcc250760b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
